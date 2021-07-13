340fc420424a7fff301d0ad03ab663bbd9e25457
joeljhou <2371425251@qq.com>
2021-07-13 22:16:37 +0800

    springboot04-jdbctemplate

--- mayikt/SpringBoot/springboot04-jdbctemplate/src/main/java/com/mayikt/Springboot04JdbctemplateApplication.java
+++ mayikt/SpringBoot/springboot04-jdbctemplate/src/main/java/com/mayikt/Springboot04JdbctemplateApplication.java
@@ -1,4 +1,4 @@
-package com.mayikt.springboot04jdbctemplate;
+package com.mayikt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ mayikt/SpringBoot/springboot04-jdbctemplate/src/main/java/com/mayikt/service/UserService.java
@@ -0,0 +1,14 @@
+package com.mayikt.service;
+
+/**
+ * @author 周宇
+ * @create 2021-07-13 21:55
+ */
+public interface UserService {
+
+    /**
+     * 插入数据到User库中
+     */
+    public Boolean inserUser(String name,Integer age);
+
+}

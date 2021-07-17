f942e311be8affa40c0a9efeaacc70e2cb9acddc
joeljhou <2371425251@qq.com>
2021-07-18 00:15:31 +0800

    Order订单服务——调用——>Member会员服务 通过注入RestTemplate和LoadBalanced注解实现动态负载均衡

--- mayikt/SpringCloud/springcloud01-eureak-server/src/main/java/com/mayikt/AppEureakService.java
+++ mayikt/SpringCloud/springcloud01-eureak-server/src/main/java/com/mayikt/AppEureakService.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//开启eurekaServer服务 开启注册中心
+//开启eurekaServer服务端 发现其他服务
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringcloudEureakServerApplication {
+public class AppEureakService {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudEureakServerApplication.class, args);
+        SpringApplication.run(AppEureakService.class, args);
     }
 
 }

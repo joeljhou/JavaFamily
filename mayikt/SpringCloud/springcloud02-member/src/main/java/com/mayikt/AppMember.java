e0da1556c0c6cc39824c327062ec619f839960ce
joeljhou <2371425251@qq.com>
2021-07-18 00:16:33 +0800

    Order订单服务——调用——>Member会员服务 通过注入RestTemplate和LoadBalanced注解实现动态负载均衡

---
+++ mayikt/SpringCloud/springcloud02-member/src/main/java/com/mayikt/AppMember.java
@@ -0,0 +1,16 @@
+package com.mayikt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//开启eurekaClient客户端 将服务注册到eureak服务端
+@EnableEurekaClient
+@SpringBootApplication
+public class AppMember {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMember.class, args);
+    }
+
+}
---
+++ mayikt/SpringCloud/springcloud02-member/src/test/java/com/mayikt/SpringcloudMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayikt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

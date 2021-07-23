4037bd532d413a4e37700cd38cf3803b0408c894
joeljhou <2371425251@qq.com>
2021-07-23 23:51:14 +0800

    多个后置处理器通过实现Ordered处理那个先执行 值越小，越先执行

--- mayikt/Spring5/mayikt-spring5/src/main/java/com/mayikt/bean/MayiktBeanPost02.java
+++ mayikt/Spring5/mayikt-spring5/src/main/java/com/mayikt/bean/MayiktBeanPost02.java
@@ -7,9 +7,9 @@ import org.springframework.core.Ordered;
 /**
  * @author 周宇
  * @create 2021-07-23 23:36
- * Bean后置处理器
+ * Bean后置处理器 配置多个后置处理器 那个先执行
  */
-public class MayiktBeanPost implements BeanPostProcessor, Ordered {
+public class MayiktBeanPost02 implements BeanPostProcessor, Ordered {
 
     /**
      * 调用初始化方法之前执行
@@ -20,7 +20,7 @@ public class MayiktBeanPost implements BeanPostProcessor, Ordered {
      */
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("【后置处理器】 Bean Before");
+        System.out.println("【后置处理器02】 Bean Before");
         return bean;
     }
 
@@ -33,13 +33,13 @@ public class MayiktBeanPost implements BeanPostProcessor, Ordered {
      */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("【后置处理器】 Bean After");
+        System.out.println("【后置处理器02】 Bean After");
         return bean;
     }
 
-
+    //值越小，越先执行
     @Override
     public int getOrder() {
-        return 1;
+        return 2;
     }
 }

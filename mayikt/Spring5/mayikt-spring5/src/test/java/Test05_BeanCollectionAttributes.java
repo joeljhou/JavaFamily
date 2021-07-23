cb75c07e2c61e6099dfac043a961c43c09db045c
joeljhou <2371425251@qq.com>
2021-07-23 22:28:04 +0800

    bean注入集合

--- mayikt/Spring5/mayikt-spring5/src/test/java/Test05_BeanCollectionAttributes.java
+++ mayikt/Spring5/mayikt-spring5/src/test/java/Test05_BeanCollectionAttributes.java
@@ -8,10 +8,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Test05_BeanCollectionAttributes {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring_collection.xml");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("collection.xml");
         //注入外部bean
         StuEntity stuEntity = applicationContext.getBean("stuEntity", StuEntity.class);
         System.out.println(stuEntity);
+        //对list公共部分进行提取
+        StuEntity stuEntity2 = applicationContext.getBean("stuEntity2", StuEntity.class);
+        System.out.println(stuEntity2);
         System.out.println("=============注入属性外部bean Success=============");
     }
 }

3a4e83eca19791a6515c6788dc6dfbf05fe5d601
joeljhou <2371425251@qq.com>
2021-08-10 18:02:50 +0800

    Nacos构建配置中文

---
+++ mayikt/SpringCloud Alibaba/004.NacosCluster/mayikt_nacos_client/src/main/java/com/mayikt/service/NacosController.java
@@ -0,0 +1,25 @@
+package com.mayikt.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 周宇
+ * @create 2021-08-10 17:26
+ */
+@RestController
+@RefreshScope
+public class NacosController {
+
+    @Value("${mayikt.name}")
+    private String userName;
+
+    @RequestMapping("/getConfig")
+    public String getConfig() {
+        return userName;
+    }
+
+}
---
+++ mayikt/SpringCloud Alibaba/004.NacosCluster/mayikt_nacos_client/src/test/java/com/mayikt/mayikt_nacos_client/MayiktNacosClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayikt.mayikt_nacos_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayiktNacosClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

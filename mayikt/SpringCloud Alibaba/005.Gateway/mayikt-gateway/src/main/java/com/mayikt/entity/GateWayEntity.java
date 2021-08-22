57fc7cc9a8edc6e9d3e799e8901909a3a185648e
joeljhou <2371425251@qq.com>
2021-08-23 01:04:57 +0800

    Gateway实现动态路由

---
+++ mayikt/SpringCloud Alibaba/005.Gateway/mayikt-gateway/src/main/java/com/mayikt/entity/GateWayEntity.java
@@ -0,0 +1,8 @@
+package com.mayikt.entity;
+
+/**
+ * @author 周宇
+ * @create 2021-08-23 1:02
+ */
+public class GateWayEntity {
+}
---
+++ mayikt/SpringCloud Alibaba/005.Gateway/mayikt-gateway/src/main/java/com/mayikt/service/GatewayService.java
@@ -0,0 +1,27 @@
+package com.mayikt.service;
+
+import com.mayikt.mapper.MayiktGatewayMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 周宇
+ * @create 2021-08-23 0:54
+ */
+@Component
+public class GatewayService implements ApplicationEventPublisherAware {
+    private ApplicationEventPublisher publisher;
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+    @Autowired
+    private MayiktGatewayMapper mayiktGateway;
+
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+
+    }
+}

c55d69a35a83d5a86312bc8e071eb87b46c563b7
joeljhou <2371425251@qq.com>
2021-09-12 18:58:20 +0800

    基于Zookeeper事件通知实现动态负载均衡 (后面再研究)

---
+++ mayikt/Dubbo/dubbo-load/src/main/java/com/mayikt/server/ServerHandler.java
@@ -0,0 +1,60 @@
+package com.mayikt.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author 周宇
+ * @create 2021-09-12 2:39
+ * Server处理线程
+ */
+public class ServerHandler implements Runnable{
+
+    private Socket socket;
+
+    public ServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            out = new PrintWriter(this.socket.getOutputStream(), true);
+            String body = null;
+            while (true) {
+                body = in.readLine();
+                if(body == null) {
+                    break;
+                }
+                System.out.println("Receive : " + body);
+                out.println("Hello, " + body);
+            }
+
+        } catch (Exception e) {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (this.socket != null) {
+                try {
+                    this.socket.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                this.socket = null;
+            }
+        }
+    }
+}

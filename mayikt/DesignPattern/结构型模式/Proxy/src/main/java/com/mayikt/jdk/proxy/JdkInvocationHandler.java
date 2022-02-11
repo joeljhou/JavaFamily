d8ac4ae0ca257d2d5e07853c5437376553b89776
joeljhou <2371425251@qq.com>
2022-02-11 16:02:40 +0800

    JDK动态代理 添加注释

--- mayikt/DesignPattern/结构型模式/Proxy/src/main/java/com/mayikt/jdk/proxy/JdkInvocationHandler.java
+++ mayikt/DesignPattern/结构型模式/Proxy/src/main/java/com/mayikt/jdk/proxy/JdkInvocationHandler.java
@@ -11,25 +11,20 @@ import java.lang.reflect.Proxy;
  */
 public class JdkInvocationHandler implements InvocationHandler {
     /**
-     * 目标对象
+     * 被代理的目标对象
      */
     private Object target;
-
-    /**
-     * 构造方法，给我们要代理的真实对象赋初值
-     */
     public JdkInvocationHandler(Object target) {
         this.target = target;
     }
 
     /**
+     * 处理代理实例，并返回结果
      * 该方法负责集中处理动态代理类上的所有方法调用
      * 调用处理器根据这三个参数进行预处理或分派到委托类实例上反射执行
-     *
      * @param proxy  代理类实例
      * @param method 目标方法
      * @param args   方法需要传递的参数
-     * @return
      */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -39,6 +34,9 @@ public class JdkInvocationHandler implements InvocationHandler {
         return result;
     }
 
+    /**
+     * 生成得到代理类
+     */
     public <T> T getProxy() {
         return (T) Proxy.newProxyInstance(target.getClass().getClassLoader(),
                 target.getClass().getInterfaces(), this);

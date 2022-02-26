685ee575d8de3a7c32f217708cc135cdca6dc227
joeljhou <2371425251@qq.com>
2022-02-26 12:48:01 +0800

    创建redis01-jedis的SpringBoot项目

---
+++ mayikt/Redis/redis01-jedis/src/main/java/com/xhgj/Redis01JedisApplication.java
@@ -0,0 +1,13 @@
+package com.xhgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis01JedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Redis01JedisApplication.class, args);
+    }
+
+}
---
+++ mayikt/Redis/redis01-jedis/src/test/java/com/xhgj/Redis01JedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhgj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis01JedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4d2290103a257dc2909ad271c649a00a171be527
joeljhou <2371425251@qq.com>
2022-03-08 00:26:56 +0800

    java 锁机制

---
+++ Juc/javaLock/src/main/java/com/xhgj/JavaLockApplication.java
@@ -0,0 +1,13 @@
+package com.xhgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLockApplication.class, args);
+    }
+
+}
---
+++ Juc/javaLock/src/test/java/com/xhgj/JavaLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhgj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
